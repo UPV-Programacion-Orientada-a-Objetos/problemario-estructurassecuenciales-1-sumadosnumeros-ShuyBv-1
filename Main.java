d4a29bfdd81d1475b2f8d73ee78d5feb6bf33267
Jesus_Antonio_Olazaran_Mora <2130035@upv.edu.mx>
2023-05-11 11:29:26 -0600

    Version 1.0.- Funcional

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
-
     public static void main(String[] args) {
+        //SumaDosNumeros obj = new SumaDosNumeros(); //Con new se genera un espacio en memoria.
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
+        SumaDosNumeros obj = new SumaDosNumeros();
+
+        System.out.println(obj.suma(a, b));
     }
 }
